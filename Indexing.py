db42026e81adce9556b1b5bb7af3a71fe10205ec
MenacingDwarf <MenacingDwarf@yandex.ru>
2021-12-06 20:33:16 +0300

    Elias and check image/document

--- Indexing.py
+++ Indexing.py
@@ -10,15 +10,25 @@ class Indexing:
     def __init__(self):
         self.urls_ids = {}
         self.indexing_words = {}
+        self.indexing_words_elias = {}
 
     def clear_index(self):
         self.urls_ids = {}
         self.indexing_words = {}
+        self.indexing_words_elias = {}
+
+    def is_document(self, url):
+        lower_url = url.lower()
+        return lower_url.endswith('.doc') or lower_url.endswith('.docx') or lower_url.endswith('.pdf')
+
+    def is_image(self, url):
+        lower_url = url.lower()
+        return lower_url.endswith('.jpg') or lower_url.endswith('.jpeg') or lower_url.endswith('.gif') or lower_url.endswith('.svg')
 
     # Get url id from list of urls or new
     def get_url_id(self, url):
         if not self.urls_ids:
-            new_id = 0
+            new_id = 1
         else:
             max_id = max(self.urls_ids.keys())
             new_id = max_id + 1
@@ -36,8 +46,10 @@ class Indexing:
     def add_word(self, word, url_id):
         if word in self.indexing_words:
             self.indexing_words[word].append(url_id)
+            self.indexing_words_elias[word] = bitarray(self.indexing_words_elias[word].to01() + Elias_Gamma(url_id))
         else:
             self.indexing_words[word] = [url_id]
+            self.indexing_words_elias[word] = bitarray(Elias_Gamma(url_id))
 
     # Return list of urls from report file
     def parse_report(self, report_path):
@@ -55,10 +67,11 @@ class Indexing:
 
     # Get all words from url and add them to index
     def parse_url(self, url):
-        words = self.get_url_words(url)
-        url_id = self.get_url_id(url)
-        for word in words:
-            self.add_word(word, url_id)
+        if not self.is_document(url) and not self.is_image(url):
+            words = self.get_url_words(url)
+            url_id = self.get_url_id(url)
+            for word in words:
+                self.add_word(word, url_id)
 
     # Parse all urls from report file and add to index
     def add_to_index(self, report_path):
@@ -79,6 +92,7 @@ if __name__ == '__main__':
     indexing = Indexing()
     indexing.clear_index()
     indexing.add_to_index('reports/slurp.report')
-    #print(indexing.urls_ids)
-    #print(indexing.indexing_words)
+    print(indexing.urls_ids)
+    print(indexing.indexing_words)
+    print(indexing.indexing_words_elias)
     print(indexing.find_by_word("рамен"))
